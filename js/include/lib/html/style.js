27a72cb245149240b779e1637a11f240432d359e
Christoph Zorn <zornchrisjo@googlemail.com>
2018-02-26 17:02:05 +0100

    adjustments to code parser, started lib to generate dynamic html widgets

---
+++ js/include/lib/html/style.js
@@ -0,0 +1,37 @@
+var style =
+    {
+        TableWidget:
+            {
+                table:
+                    {
+                        border: "1px solid black"
+                    },
+                tr:
+                    {
+
+                    },
+                tr_hover:
+                    {
+                        background: "gray"
+                    },
+                th:
+                    {
+                        fontWeight: "bold",
+                        fontSize: "14pt"
+                    },
+                td:
+                    {
+
+                    }
+            },
+        ListWidget:
+            {
+                backgroundColor: "gray"
+            },
+        TabWidget:
+            {
+                width: "100%",
+                display: "inline",
+                float: "left"
+            }
+    };
